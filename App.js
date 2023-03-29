26cb32e4fe144817239216e84b5e67dd4eb9c065
abdulr77mn <88157374+abdulr77mn@users.noreply.github.com>
2023-03-29 22:24:52 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,46 @@
+import { StyleSheet, Text, View } from "react-native";
+import AppNavigator from "./Navigator/AppNavigator";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import ReduxThunk from "redux-thunk";
+import { Provider } from "react-redux";
+import productReducer from "./store/action/reducers/productReducer";
+import cartReducer from "./store/action/reducers/cartReducer";
+import orderReducer from "./store/action/reducers/orderReducer";
+import authReducer from "./store/action/reducers/authReducer";
+import profileReducer from "./store/action/reducers/profileReducer";
+import { LogBox } from "react-native";
+import { persistReducer, persistStore } from "redux-persist";
+
+import { PersistGate } from "redux-persist/es/integration/react";
+import AsyncStorageLib from "@react-native-async-storage/async-storage";
+import reportReducer from "./store/action/reducers/reportReducer";
+
+LogBox.ignoreAllLogs();
+
+const rootReducer = combineReducers({
+  products: productReducer,
+  cart: cartReducer,
+  orders: orderReducer,
+  auth: authReducer,
+  profile: profileReducer,
+  report: reportReducer,
+});
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorageLib,
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer, applyMiddleware(ReduxThunk));
+const persistor = persistStore(store);
+export default function App() {
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor} loading={null}>
+        <AppNavigator />
+      </PersistGate>
+    </Provider>
+  );
+}
+
+const styles = StyleSheet.create({});
